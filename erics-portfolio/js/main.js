8ff11ce7c8c6f9a4775b8f4d4cf069987899390d
ewilliams149 <ericw9968@gmail.com>
2024-12-07 12:17:44 -0700

    Pic of me changed, toggle button, border radius, contact
    - Changed about me picture
    - customized toggle button
    - change border radius of cards
    changed "contact" to "email me" in header

--- erics-portfolio/js/main.js
+++ erics-portfolio/js/main.js
@@ -1,4 +1,9 @@
 function toggleMenu() {
     const navbar = document.querySelector('.navbar');
+    const menuToggle = document.querySelector('.menu-toggle');
+    
     navbar.classList.toggle('show');
+
+    menuToggle.classList.toggle('open');
 }
+
